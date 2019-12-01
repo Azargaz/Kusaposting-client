d120001d1b168e58cafe5e69e4f1695ba22e7a37
azargaz <huba310@gmail.com>
2019-12-01 18:10:34 +0100

    Added signup and authorization state.

--- src/App.js
+++ src/App.js
@@ -5,31 +5,32 @@ import "./App.css";
 
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles/';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
 // components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 // pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: '#48a999',
-			main: '#00796b',
-			dark: '#004c40',
-			contrastText: '#ffffff',
-		},
-		secondary: {
-			light: '#a98274',
-			main: '#795548',
-			dark: '#4b2c20',
-			contrastText: '#ffffff',
-		}
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.firebaseIdToken;
+
+if(token) {
+	const decodedToken = jwtDecode(token);
+
+	if(decodedToken.exp * 1000 < Date.now()) {
+		window.location.href = '/login';
+		authenticated = false;
+	} else {
+		authenticated = true;
 	}
-});
+}
 
 function App() {
 	return (
@@ -40,8 +41,16 @@ function App() {
 					<div className="container">					
 						<Switch>
 							<Route exact path="/" component={home} />
-							<Route exact path="/login" component={login} />
-							<Route exact path="/signup" component={signup} />
+							<AuthRoute 
+								exact path="/login" 
+								component={login} 
+								authenticated={authenticated}
+							/>
+							<AuthRoute 
+								exact path="/signup" 
+								component={signup} 
+								authenticated={authenticated}
+							/>
 						</Switch>
 					</div>
 				</Router>
--- src/pages/login.js
+++ src/pages/login.js
@@ -12,35 +12,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '20px auto 20px auto',
-        width: '200px',
-        height: '200px',
-        border: 'solid 1px black'
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: '10'
-    },
-    progress: {
-        position: 'absolute'
-    }
-};
+const styles = (theme) => ({
+    ...theme.styles
+});
 
 class login extends Component {
     constructor() {
@@ -66,6 +40,7 @@ class login extends Component {
         axios.post('/login', userData)
             .then(res => {
                 console.log(res.data);
+                localStorage.setItem('firebaseIdToken', `Bearer ${res.data.token}`);
                 this.setState({
                     loading: false
                 });
