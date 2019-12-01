32d29cabef86dc1ecd8096d517b990a58df5df7b
azargaz <huba310@gmail.com>
2019-12-01 14:44:15 +0100

    Added kusoposts to the homepage.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles/';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 // components
 import Navbar from './components/Navbar';
 
@@ -10,20 +13,39 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#48a999',
+			main: '#00796b',
+			dark: '#004c40',
+			contrastText: '#ffffff',
+		},
+		secondary: {
+			light: '#a98274',
+			main: '#795548',
+			dark: '#4b2c20',
+			contrastText: '#ffffff',
+		}
+	}
+});
+
 function App() {
 	return (
-		<div className="App">
-			<Router>
-				<Navbar />
-				<div className="container">					
-					<Switch>
-						<Route exact path="/" component={home} />
-						<Route exact path="/login" component={login} />
-						<Route exact path="/signup" component={signup} />
-					</Switch>
-				</div>
-			</Router>
-		</div>
+		<MuiThemeProvider theme={theme}>
+			<div className="App">
+				<Router>
+					<Navbar />
+					<div className="container">					
+						<Switch>
+							<Route exact path="/" component={home} />
+							<Route exact path="/login" component={login} />
+							<Route exact path="/signup" component={signup} />
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		</MuiThemeProvider>		
 	);
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import Kusopost from '../components/Kusopost';
 
 class home extends Component {
+    state = {
+        kusoposts: null
+    }
+    
+    componentDidMount() {
+        axios.get('/kusoposts')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    kusoposts: res.data
+                })
+            })
+            .catch(err => console.error(err));
+    }
+
     render() {
+        let recentKusopostsMarkup = this.state.kusoposts ? (
+            this.state.kusoposts.map((kusopost, id) => <Kusopost key={id} kusopost={kusopost} />)
+        ) : <p>Loading...</p>;
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <Grid container spacing={10}>
+                <Grid item sm={8} xs={12}>
+                    {recentKusopostsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
