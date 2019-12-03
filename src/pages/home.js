072fd120adb835c3697c45941f9ab0da3e355f93
azargaz <huba310@gmail.com>
2019-12-03 21:58:26 +0100

    Added profile section to homepage.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 import Kusapost from '../components/Kusapost';
+import Profile from '../components/Profile'
 
 class home extends Component {
     state = {
@@ -30,7 +31,7 @@ class home extends Component {
                     {recentkusapostsMarkup}
                 </Grid>
                 <Grid item sm={4} xs={12}>
-                    <p>Profile...</p>
+                    <Profile />
                 </Grid>
             </Grid>
         )
